417ddd04be5e13c944387232866c41d982bf0339
alexnesov <alexandrenesovic@gmail.com>
2020-09-22 09:32:48 +0200

    improvements

--- Functions/lireetdire.cc
+++ Functions/lireetdire.cc
@@ -17,43 +17,85 @@ int separer_chiffre_gauche(int& nombre)
  * Compléter le code à partir d'ici
  *****************************************************/
 int chiffre(0);
-int tempone(0);
+
 int old_left;
+int new_left;
+
 int newnew;
+int repetitions_chiffre(1);
+int temptwo;
+int lire_et_dire(int nombre);
 
 
 void ajouter_chiffre_droit(int& nombre, int chiffre)
 {
-  newnew = tempone * 10 + old_left;
-  cout << "NEW NEW : " << newnew << endl;
+  newnew = repetitions_chiffre * 10 + old_left;
+  cout << "ajouter_chiffre_droit : " << newnew << endl;
+
+  // RE-INITILIZATION
+  repetitions_chiffre = 0;
+  old_left = new_left;
 }
 
 void dire_chiffre(int& nombre, int repetitions_chiffre, int chiffre)
 { 
+
+/*   ajouter_chiffre_droit(nombre, chiffre);
+ */  // isoler gauche
+  // placer dans nombre
+  // ajouter repetitions à nombre
+  // ajouter chiffre à nombre
+
+  newnew = newnew * 10 + new_left;
+  cout << "Dire_chiffre: " << newnew << endl;
+  
+
+  cout << "NOMBRE : " << temptwo << endl;
   nombre = lire_et_dire(nombre);
+
 }
 
 int lire_et_dire(int nombre)
 {
-  int new_left;
 
-  old_left = separer_chiffre_gauche(nombre);
-  
-  do{
-    new_left = separer_chiffre_gauche(nombre);
-    tempone = tempone +1;
-  }while (new_left == old_left);
+while (nombre !=0)
+    {
+
+      void calculer_repetitions(int nombre)
+      {
+          // repetant jusqu'a ce que le nombre manipulé soit nul
+            old_left = separer_chiffre_gauche(nombre);
+
+            cout << "OLD LEFT: " << old_left << endl;
+
+            new_left = separer_chiffre_gauche(nombre);
+
+            while (new_left == old_left)
+            {
+            
+            new_left = separer_chiffre_gauche(nombre);
+            cout << "NEW LEFT: " << new_left << endl;
+
+            cout << "NOMBRE : " << nombre << endl;
+            repetitions_chiffre = repetitions_chiffre +1;
+            cout << " REPETITION " << repetitions_chiffre << endl;
+            }
+      }
 
 
+      calculer_repetitions(nombre);
+      cout << "chiffre old :" << old_left << endl;
+      cout << "___nb of " << old_left << " : " << repetitions_chiffre << endl;
+      cout << "chiffre new :" << new_left << endl;
+      ajouter_chiffre_droit(nombre,repetitions_chiffre);
 
-  cout << "chiffre old :" << old_left << endl;
-  cout << "nb of " << old_left << " : " << tempone << endl;
-  cout << "chiffre new :" << new_left << endl;
-  ajouter_chiffre_droit(nombre,tempone);
+      temptwo = nombre;
+      cout << "temptwo : " << temptwo << endl;
+      dire_chiffre(nombre,repetitions_chiffre,new_left);
+    }
+      return nombre;
 
-  return nombre;
 }
- 
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
