c1aeed759d50065210b4d1e283ff95c8ae2c60be
alexnesov <alexandrenesovic@gmail.com>
2020-09-22 18:31:48 +0200

    comment

--- Functions/lireetdire.cc
+++ Functions/lireetdire.cc
@@ -17,12 +17,8 @@ int separer_chiffre_gauche(int& nombre)
  * Compléter le code à partir d'ici
  *****************************************************/
 
-int temp2;
-int temp3;
 int resultat(0);
-int chiffre;
-int repetitions_chiffre(1);
-bool init(true);
+
 
 void ajouter_chiffre_droit(int& nombre, int chiffre)
 {
@@ -33,41 +29,71 @@ void dire_chiffre(int& nombre, int repetitions_chiffre, int chiffre)
 {
   resultat = resultat * 10 + repetitions_chiffre;
   ajouter_chiffre_droit(nombre, chiffre);
-
-  // Re-initilization
-  repetitions_chiffre = 0;
 }
 
 int lire_et_dire(int nombre)
 {
 
-  // jusqu'a ce que le nombre manipulé soit nul
+
+  int temp2;
+  int temp3;
+  int diff;
+  int memory;
+  int chiffre;
+  int repetitions_chiffre(1);
+  bool init = true;
+  bool previous_multiple = false;
 
   while(nombre!=0)
   {
-
-    temp2 = separer_chiffre_gauche(nombre);
-    temp3 = separer_chiffre_gauche(nombre);
-      
-    if (temp2==temp3)
+    cout << endl;
+    cout << endl;
+    
+    if(init==true)
     {
-      cout << "rep test" << repetitions_chiffre << endl;
-      repetitions_chiffre++;
-    }
+          temp2 = separer_chiffre_gauche(nombre);
+          cout << "TEMP2: " << temp2 << endl;
 
-    if (temp3==temp2)
-    {
-      repetitions_chiffre++;
+          temp3 = separer_chiffre_gauche(nombre);
+          cout << "TEMP3: " << temp3 << endl;
+
+          init = false;
     }
     else
     {
-      chiffre=temp2;
-      cout << "test" << endl;
-      dire_chiffre(nombre, repetitions_chiffre, chiffre);
+          temp2 = temp3;
+          temp3 = separer_chiffre_gauche(nombre);
     }
-    
-    cout << "TEMP2: " << temp2 << endl;
-    cout << "TEMP3: " << temp3 << endl;
+
+    cout << "INIT : " << init << endl;
+    diff = temp3 - temp2;
+    cout << "diff : " << diff << endl;
+
+
+    if(diff==0)
+    {
+        previous_multiple = true;
+        memory = temp2;
+        repetitions_chiffre++;
+    }
+    else
+    {
+        if(previous_multiple==true)
+        {
+            dire_chiffre(nombre,repetitions_chiffre,chiffre=memory);
+            
+        }
+        else
+        {
+            dire_chiffre(nombre,repetitions_chiffre,chiffre=temp3);
+        }
+        
+        // Re-initilization
+        repetitions_chiffre = 1;
+        previous_multiple = false;
+    }
+        
+    cout << "TEMP 3 : " << temp3 << endl;
   }
 
   return resultat;
