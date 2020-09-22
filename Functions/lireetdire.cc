4654c611f15b9671d280d984893bfde2e1f87ac5
alexnesov <alexandrenesovic@gmail.com>
2020-09-22 17:10:24 +0200

    algo

--- Functions/lireetdire.cc
+++ Functions/lireetdire.cc
@@ -16,86 +16,55 @@ int separer_chiffre_gauche(int& nombre)
 /*****************************************************
  * Compléter le code à partir d'ici
  *****************************************************/
-int chiffre(0);
 
-int old_left;
-int new_left;
-
-int newnew;
+int temp2;
+int temp3;
+int repeated;
+bool repeated_act();
+int resultat(0);
+int chiffre;
 int repetitions_chiffre(1);
-int temptwo;
-int lire_et_dire(int nombre);
-
 
 void ajouter_chiffre_droit(int& nombre, int chiffre)
 {
-  newnew = repetitions_chiffre * 10 + old_left;
-  cout << "ajouter_chiffre_droit : " << newnew << endl;
-
-  // RE-INITILIZATION
-  repetitions_chiffre = 0;
-  old_left = new_left;
+  resultat = resultat * 10 + chiffre;
 }
 
 void dire_chiffre(int& nombre, int repetitions_chiffre, int chiffre)
-{ 
-
-/*   ajouter_chiffre_droit(nombre, chiffre);
- */  // isoler gauche
-  // placer dans nombre
-  // ajouter repetitions à nombre
-  // ajouter chiffre à nombre
-
-  newnew = newnew * 10 + new_left;
-  cout << "Dire_chiffre: " << newnew << endl;
-  
-
-  cout << "NOMBRE : " << temptwo << endl;
-  nombre = lire_et_dire(nombre);
-
+{
+  resultat = resultat * 10 + repetitions_chiffre;
+  ajouter_chiffre_droit(nombre, chiffre);
 }
 
 int lire_et_dire(int nombre)
 {
 
-while (nombre !=0)
+  // jusqu'a ce que le nombre manipulé soit nul
+
+  while(nombre!=0)
+  {
+    temp2 = separer_chiffre_gauche(nombre);
+    temp3 = separer_chiffre_gauche(nombre);
+    
+    if (temp2==temp3)
     {
-
-      void calculer_repetitions(int nombre)
-      {
-          // repetant jusqu'a ce que le nombre manipulé soit nul
-            old_left = separer_chiffre_gauche(nombre);
-
-            cout << "OLD LEFT: " << old_left << endl;
-
-            new_left = separer_chiffre_gauche(nombre);
-
-            while (new_left == old_left)
-            {
-            
-            new_left = separer_chiffre_gauche(nombre);
-            cout << "NEW LEFT: " << new_left << endl;
-
-            cout << "NOMBRE : " << nombre << endl;
-            repetitions_chiffre = repetitions_chiffre +1;
-            cout << " REPETITION " << repetitions_chiffre << endl;
-            }
-      }
-
-
-      calculer_repetitions(nombre);
-      cout << "chiffre old :" << old_left << endl;
-      cout << "___nb of " << old_left << " : " << repetitions_chiffre << endl;
-      cout << "chiffre new :" << new_left << endl;
-      ajouter_chiffre_droit(nombre,repetitions_chiffre);
-
-      temptwo = nombre;
-      cout << "temptwo : " << temptwo << endl;
-      dire_chiffre(nombre,repetitions_chiffre,new_left);
+      repetitions_chiffre++;
     }
-      return nombre;
+    else
+    {
+      chiffre=temp2;
+      dire_chiffre(nombre, repetitions_chiffre, chiffre);
+    }
+    
+    cout << "TEMP2: " << temp2 << endl;
+    cout << "TEMP3: " << temp3 << endl;
+  }
 
+  cout << repetitions_chiffre << endl;
+
+  return resultat;
 }
+
 /*******************************************
  * Ne rien modifier après cette ligne.
  *******************************************/
