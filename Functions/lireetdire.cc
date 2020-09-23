594d08a7dd11f8d26ba1a9f474f957ce6b640f18
alexnesov <alexandrenesovic@gmail.com>
2020-09-23 16:08:12 +0200

    working algo

--- Functions/lireetdire.cc
+++ Functions/lireetdire.cc
@@ -17,10 +17,8 @@ int separer_chiffre_gauche(int& nombre)
  * Compléter le code à partir d'ici
  *****************************************************/
 
-
-
 int repetition;
-int resultat;
+int nombre_init;
 int chiffre_init;
 int chiffre;
 int new_chiffre;
@@ -28,59 +26,48 @@ bool init = true;
 
 void ajouter_chiffre_droit(int& nombre, int chiffre)
 {
-  resultat = resultat * 10 + chiffre;
+  nombre = nombre * 10 + chiffre;
 }
 
 void dire_chiffre(int& nombre, int repetitions_chiffre, int chiffre)
 {
-  resultat = resultat * 10 + repetition;
+  nombre = nombre * 10 + repetition;
   ajouter_chiffre_droit(nombre, chiffre);
   repetition = 1;
 }
 
 int lire_et_dire(int nombre)
 {
-  cout << endl;
-  bool multi;
   int repetitions_chiffre;
+  nombre_init = nombre;
+  nombre = 0;
 
-  chiffre = separer_chiffre_gauche(nombre);
-  cout << "CHIFFRE : " << chiffre << endl;
+  chiffre = separer_chiffre_gauche(nombre_init);
 
   do
   {
     do
     {
-      new_chiffre = separer_chiffre_gauche(nombre);
+      new_chiffre = separer_chiffre_gauche(nombre_init);
       repetition++;
     } while (chiffre==new_chiffre);
 
-    cout << "CHIFFRE : " << chiffre << endl;
-    cout << "NEW CHIFFRE : " << new_chiffre << endl;
-    cout << "REPETITION : " << repetition << endl;
-    cout << "NOMBRE : " << nombre << endl;
-    cout << endl;
-
     dire_chiffre(nombre, repetitions_chiffre=repetition, chiffre);
 
-    if((nombre==0)&&(new_chiffre!=0))
+    if((nombre_init==0)&&(new_chiffre!=0))
     // Second condition for
     // when n=0 AND digit alone at the end of the number: i.e "11221"
     {
       dire_chiffre(nombre, repetitions_chiffre=1,chiffre=new_chiffre);
     }
 
-
-
-
-
     // Re-initilizations
     repetition=0;
     chiffre=new_chiffre;
 
-  } while (nombre!=0);
+  } while (nombre_init!=0);
   
-  return resultat;
+  return nombre;
 
 }
 
