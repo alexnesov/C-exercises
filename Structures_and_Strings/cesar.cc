5543cb24cd3402f227d1548ee42bf9ecb2ca6f0b
alexnesov <alexandrenesovic@gmail.com>
2020-10-03 16:16:20 +0200

    cesar

---
+++ Structures_and_Strings/cesar.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+char decale(char c, char debut, int decalage)
+{
+    char new_char;
+
+    while(decalage<0)
+    {
+        decalage+26;
+    }
+    
+    new_char = debut + (((c - debut) + decalage) % 26);
+    return new_char; 
+}
+
+
+char code(char initialChar, int d)
+{
+    char dec;
+
+    if((initialChar >= 'a') && (initialChar<='z'))
+    {
+        d = decale(initialChar, 'a', d);
+        return d;
+    }
+    else if ((initialChar >='A') && (initialChar <= 'Z'))
+    {
+        d = decale(initialChar, 'A', d);
+        return d;
+    }
+    else
+    {
+        return initialChar;
+    } 
+
+}
+
+string code(string initialString, int d)
+{
+
+    string final_string;
+
+    for(size_t i(0); i <= initialString.size(); i++)
+    {
+        char old_char;
+        char new_char;
+
+
+        old_char = initialString[i];
+        new_char = code(old_char,d);
+        final_string.push_back(new_char);
+    }
+
+    return final_string;
+    
+}
+
+
+
+int main()
+{
+    string initialString;
+    cin >> initialString;
+    return 0;
+}
