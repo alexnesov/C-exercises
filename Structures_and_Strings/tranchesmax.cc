e0194ba953457aa3c0437c8495d2e0a345b008b1
alexnesov <alexandrenesovic@gmail.com>
2020-10-05 14:53:30 +0200

    success for 'somme_cons_max'

--- Structures_and_Strings/tranchesmax.cc
+++ Structures_and_Strings/tranchesmax.cc
@@ -1,32 +1,73 @@
-#include <vector>
 #include <iostream>
-#include <string>
+#include <vector>
+using namespace std;
 
-std::vector<std::vector<int>> table(
-{   {0,1,2,0,1 },
-    {0,1,1,1,0 },
-    {3,0,0,0,2 },
-    {1,0,2,3,0 } }
+
+vector<vector<int>> table(
+{   {0,0,5,0,1  },
+    {2,0,1,3,0  },
+    {3,1,0,2,0  },
+    {1,0,1,0,2  } }
 );
 
-int iterate_over_vector(std::vector<std::vector<int>> v)
-{
 
-    int sum;
-    int best_sum;
+int somme_consecutifs_max(vector<int> v)
+{
+    int sum(0);
+    int biggestSum(0);
+    
+    int previous_temp(0);
 
     for(auto i : v)
     {
-        for(auto j : i)
+        
+    sum = previous_temp + i;
+
+    cout << endl;
+    cout << "I°: " << i << endl;
+    cout << "SUM: " << sum << endl;
+    cout << "PREVIOUS_TEMP: " << previous_temp << endl;
+    cout << "BIGGESTSUM: " << biggestSum << endl;
+    
+        if( (i==0) )
         {
-        std::cout << j << std::endl;
+            if(sum > biggestSum)
+            {
+                biggestSum = sum;
+            }
+
+            sum = 0;
+
         }
+
+        if (sum > biggestSum)
+        {
+            biggestSum = sum;
+        }
+        
+    previous_temp = i;
+
     }
-    return best_sum;
+
+    return biggestSum;
 }
+
 
 int main()
 {
-    iterate_over_vector(table);
-    return 0;
+    int biggestSum_final;
+    vector<int> v({1,0,3,2,0});
+
+    cout << "INITIAL VECTOR: " << endl;
+    for(auto j : v)
+    {
+        cout << j;
+    }
+
+    cout << endl;
+
+    biggestSum_final = somme_consecutifs_max(v);
+
+    cout << biggestSum_final << endl;
+    
 }
