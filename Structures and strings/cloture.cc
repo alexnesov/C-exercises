04383fd4d3efa6d36502b88ed7126297446d0d00
alexnesov <alexandrenesovic@gmail.com>
2020-10-10 16:39:22 +0200

    length ok

--- Structures and strings/cloture.cc
+++ Structures and strings/cloture.cc
@@ -42,7 +42,7 @@ bool binaire(Carte const& carte)
                 }
             }
         }
-    }
+    } // For loop
                 return ok;
 }
 
@@ -84,8 +84,6 @@ void replace_lakes(vector<vector<int>>& carte, vector<vector<int>> all_difs)
   {
     if(find(empty_vec_index.begin(), empty_vec_index.end(),l) != empty_vec_index.end())
     {
-      cout << endl;
-      cout << "FOUND!" << endl;
       continue;
     }
 
@@ -95,14 +93,13 @@ void replace_lakes(vector<vector<int>>& carte, vector<vector<int>> all_difs)
     } 
   }
 
-
-
 } 
+
+vector<vector<int>> all_difs;
 
 bool verifie_et_modifie(Carte& carte)
 {
   vector<int> indices;
-  vector<vector<int>> all_difs;
   bool init(true);
   int prev(0);
   int vec_col(carte[0].size());
@@ -177,7 +174,6 @@ bool verifie_et_modifie(Carte& carte)
     }
 
     return true;
-
   }
 }
 
@@ -196,6 +192,148 @@ void affiche(Carte const& carte)
   cout << endl;
 }
 
+#include <algorithm>    // std::reverse
+
+double longueur_cloture(Carte const& carte, double echelle = 2.5)
+{
+  int index(0);
+  int beg(0);
+  int end(0);
+  int vec_line(carte.size());
+
+  vector<int> tup;
+  vector<int> reversed;
+  vector<vector<int>> all_tups;
+
+  for(auto line : carte)
+  {
+    // Find index of first one
+    // Not-rev
+    for(auto col : line)
+    {  
+      if(col==1)
+      {
+        tup.push_back(index);
+        break;
+      }
+      index++;
+    }
+    index=0;
+
+    // Find index of last one
+    // Rev
+    reverse(line.begin(),line.end());
+
+    for(auto col : line)
+    {
+      if(col==1)
+      {
+        tup.push_back(index);
+        break;
+      }
+      index++;
+    }
+
+    index=0;
+    all_tups.insert(all_tups.end(),tup);
+    tup.clear();
+
+  } // LOOP
+
+  int prev_left(0);
+  int prev_right(0);
+  bool init(true);
+  int sum(0);
+  int left(0);
+  int right(0);
+
+  cout << endl;
+  for(auto i : all_tups)
+  {
+      if(init==true)
+      {
+        prev_left = i[0];
+        prev_right = i[1];
+        init = false;
+      }
+      else
+      {
+
+
+        left = i[0] - prev_left;
+        right = i[1] - prev_right;
+
+        if(left<0)
+        {
+          left = left *-1;
+        }
+
+        if(right<0)
+        {
+          right = right *-1;
+        }
+      }
+
+      sum = sum + right + left;
+      
+      cout << i[0];
+      cout << i[1] << endl;
+
+      prev_left = i[0];
+      prev_right = i[1];
+
+  }
+  cout << endl;
+  cout << "SUM: " << sum;
+  cout << endl;
+  double length(0);
+  int vec_col(carte[0].size());
+
+  int top_carte(0);
+  int bottom_carte(0);
+
+  for(auto top:carte[0])
+  {
+    if(top==1)
+    {
+      top_carte++;
+    }
+  }
+
+  for(auto bottom:carte.back())
+  {
+    if(bottom==1)
+    {
+      bottom_carte++;
+    }
+  }
+
+  cout << "TOP CARTE: " << top_carte << endl;
+  cout << "BOTTOM CARTE: " << bottom_carte << endl;
+
+  length = vec_line * 2 + sum + top_carte + bottom_carte;
+  return length;
+} 
+
+
+void ajoute_unique(vector<int>& ensemble, int valeur)
+{
+
+}
+
+
+bool convexite_lignes(Carte& carte, vector<int> const& labels_bords)
+{
+  return true;
+}
+
+
+bool convexite_lignes(Carte& carte)
+{
+  return true;
+}
+
+
 
 /*******************************************
  * Ne rien modifier après cette ligne.
@@ -203,7 +341,7 @@ void affiche(Carte const& carte)
 
 int main()
 {
-  Carte carte = {
+/*   Carte carte = {
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0},
     {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
     {0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -233,17 +371,24 @@ int main()
     {0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
-  };
+  }; */
 
+  Carte carte = {
+      {1,1,1,1,0,0,0,0},
+      {0,0,1,0,1,0,0,0},
+      {1,1,0,1,0,0,0,0},
+      {1,1,1,1,1,1,0,0},
+      {0,0,0,0,1,0,0,1}
+      };
 
   cout << "Carte au départ :" << endl;
   affiche(carte);
   if (verifie_et_modifie(carte)) {
     cout << "Carte après modification :" << endl;
     affiche(carte);
-/*     cout << "Il vous faut " << longueur_cloture(carte)
+    cout << "Il vous faut " << longueur_cloture(carte)
          << " mètres de clôture pour votre terrain."
-         << endl; */
+         << endl;
   }
 
   return 0;
